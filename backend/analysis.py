e6faee5546a34b7f7bc97abe05dd1288acb6e56e
Ryan Vu <ryanvu1131@gmail.com>
2025-05-31 00:52:41 -0700

    analysis to store json

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 import numpy as np
 import cv2
+import json
 
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
@@ -13,9 +14,11 @@ def draw_landmarks(image, detection):
         annotated_image,
         pose_landmarks_list,
         mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec = mp_drawing.DrawingSpec(color = (0, 255, 0), thickness = 2, circle_radius = 3),
-        connection_drawing_spec=mp_drawing.DrawingSpec(color = (255, 0, 0), thickness = 2)
+        landmark_drawing_spec = mp_drawing.DrawingSpec(color = (18, 2, 17), thickness = 2, circle_radius = 3),
+        connection_drawing_spec=mp_drawing.DrawingSpec(color = (242, 228, 241), thickness = 2)
     )
+    
+    annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
         
     return annotated_image
 
@@ -52,15 +55,34 @@ def analyze_json(video_data):
     frames = []
     landmarks = []
     
-    for frame in video_array:
+    fps = 30
+    
+    for frame_num, frame in enumerate(video_array):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = mp_pose_inst.process(rgb_frame)
+        time = frame_num / 30
         
         if results.pose_landmarks:
             annotated_frame = draw_landmarks(rgb_frame, results)
             frames.append(annotated_frame)
             
-            frame_landmarks = [(lm.x, lm.y, lm.z) for lm in results.pose_landmarks.landmark]
+            frame_landmarks = {
+                'fps': fps,
+                'frame_num': frame_num,
+                'time': time,
+                'landmarks': []
+            }
+            
+            for landmark_num, landmark in enumerate(results.pose_landmarks.landmark):
+                frame_landmarks['landmarks'].append({
+                    'landmark_num': landmark_num,
+                    'x': landmark.x,
+                    'y': landmark.y,
+                    'z': landmark.z,
+                    'visibility': landmark.visibility if hasattr(landmark, 'visibility') else None
+                })
+                    
+            
             landmarks.append(frame_landmarks)
         
     output = "temp/output.mp4"
@@ -75,5 +97,10 @@ def analyze_json(video_data):
         out.write(frame)
         
     out.release()
+    
+    landmarks_file = "temp/landmarks.json"
+    
+    with open(landmarks_file, 'w') as json_file:
+        json.dump(landmarks, json_file, indent = 2)
     
     return landmarks, output
